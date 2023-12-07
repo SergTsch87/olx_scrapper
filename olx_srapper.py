72ac21ee189c277c7f8010d1fc66d25a7454a8da
unknown <sergtsch87@gmail.com>
2023-12-07 12:50:15 +0200

    Added new lines to olx_scrapper.py and .env

---
+++ olx_srapper.py
@@ -0,0 +1,14 @@
+#!usr/bin/env
+
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import os
+from dotenv import load_dotenv
+
+
+
+if __name__ == '__main__':
+    main()
