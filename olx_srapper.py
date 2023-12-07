0c1c5b1d7864a55852e6e435529fe3d8d0913303
unknown <sergtsch87@gmail.com>
2023-12-07 12:53:43 +0200

    Added new lines to olx_scrapper.py

--- olx_srapper.py
+++ olx_srapper.py
@@ -9,6 +9,17 @@ import os
 from dotenv import load_dotenv
 
 
+def main():
+    driver = webdriver.Chrome()
+    
+    login_to_olx(driver)
+    
+    url = "https://www.olx.ua/d/uk/obyavlenie/gladilka-nerzhavyucha-gladka-z-plastikovoyu-ruchkoyu-dnipro-m-120h280-mm-IDSAHjw.html"
+    data_from_page = get_data_with_page(driver, url)
+    print(data_from_page)
+
+    driver.quit()
+
 
 if __name__ == '__main__':
     main()
